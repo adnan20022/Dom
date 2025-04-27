ba428966669f7c0cbd0f75e4127bea499314f54b
adnan20022 <mu209367@gmail.com>
2025-04-26 22:33:53 -0700

    loginuser

---
+++ script.js
@@ -0,0 +1,19 @@
+titleRef=document.getElementById("title");
+
+console.log(titleRef.innerhtml);
+titleRef.innerhtml="I have hacked your site and changed title"
+
+function loginuser(){
+    console.log("user is loggined");
+    const nameinputRef=document.getElementById("name");
+    const alertRef=document.getElementsByClassName 
+    alertRef[o].innerhtml = `your form ${namevalue} has been submitted at ${currentDate.tostring}`
+
+const currentDate=new Date() 
+}
+
+// alertRef[0.innerhtml = "your form has been submitted: " + namevalue
+
+const names=["ali", "asad","farhan","amir"];
+
+
